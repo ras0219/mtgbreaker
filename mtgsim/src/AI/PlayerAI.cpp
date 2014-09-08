d013b51c264a66e2cf59d0a9b9b421755246fc42
shadowpho <shadowp@gmail.com>
2014-09-07 18:51:54 -0700

    Player works half way.

--- mtgsim/src/AI/PlayerAI.cpp
+++ mtgsim/src/AI/PlayerAI.cpp
@@ -13,7 +13,7 @@ struct ConsoleAI : PlayerLogic{
 		std::cout << "You get an action! ";
 		std::cout << ((p == g->active_player) ? "active player" : "not active player") << std::endl;
 		std::cout << "Other side has " << other_player->hand.size() << " card in hand "
-			<< other_player->life << " life " << other_player->mana << "RBWUG1 mana" << std::endl;
+			<< other_player->life << " life " << other_player->mana << std::endl;
 		return main(g, p);
 
 
@@ -38,31 +38,31 @@ struct ConsoleAI : PlayerLogic{
 		do{
 			std::cout << "Select action! Type numberrrrr" << std::endl;
 			std::cin >> action;
-			if (action >= i)
+			if (action > i+2)
 			{
 				std::cout << "i too large. Try again!" << std::endl;
 				continue;
 			}
-			if (action == i) //show card
+			if (action == i-1) //show card
 			{
 				unsigned int sel_card = 0;
-				do { std::cin >> sel_card; } while (sel_card < (i - 1));
+				do { std::cout << "enter number of card" << std::endl; std::cin >> sel_card; } while (sel_card > (i - 1));
 				Card *cur_card = p->hand.at(sel_card);
-				std::cout << i << "." << cur_card->info().name << ", "
+				std::cout << cur_card->info().name << ", "
 					<< cur_card->info().power << "/"
 					<< cur_card->info().toughness << ", "
 					<< cur_card->info().cmc << std::endl;
 				std::cout << "Detailed mana cost:" <<
 					cur_card->info().cost.pool << std::endl;
 				std::cout << "";
-				for (unsigned int i = 0; i = cur_card->info().attrs.size(); i++)
+				for (unsigned int i = 0; i < cur_card->info().attrs.size(); i++)
 					std::cout << cur_card->info().attrs.at(i) << " ";
 				std::cout << std::endl;
 				continue;
 			}
 			else//play a card
 			{
-				Card *cur_card = p->hand.at(i);
+				Card *cur_card = p->hand.at(action);
 				std::cout << "lol playing cards hard" << std::endl;
 				continue;
 			}
